0a152bc57516ef713d0fab1c2e6387f1a6a2ea98
gyakuhannta <Ktc24a31a0039@edu.kyoto-tech.ac.jp>
2026-06-06 18:08:56 +0900

    プロフィール途中

--- my-portfolio/src/data/portfolio.ts
+++ my-portfolio/src/data/portfolio.ts
@@ -1,3 +1,5 @@
+import { Link, Links } from "react-router-dom"
+
 export interface profile{
 name:string
 nameEn: string
@@ -101,5 +103,38 @@ export const profile ={
  
     name: '中村 和雅',
     nameEn: 'nakamura kazumasa',
-    role:'',
+    role:'game creator',
+    school:'京都デザイン&テクノロジー専門学校',
+    department:'スーパーゲームクリエイター専攻',
+    graduationYear:'2028年3月卒業見込み',
+    bio:'「自分の好きが誰かの夢になること」を目指すゲームプログラマーです。チームでの活動をメインで行っていて特にプレイヤー関連のプログラムを担当して、「断罪クッキング」ではインスペクターから降ってくる食材や範囲を調整出来るようにするなどプログラマー以外も設定の把握がしやすいコードを意識しています。それだけにとどまらずマネージャー役も行っていてチームの進捗管理や会議進行なども行っており作業の細分化や意見を纏める事を得意としています。チームで活動する時にアイデアを積極的に出してこのチームに自分がいたから良いゲームが作れたと言われるような働きを意識しています。',
+    motto:'「夢は前に進む為の原動力」',
+    avatar:null,
+    Links:{
+        github:'https://github.com/gyakuhannta',
+        twitter:'null',
+        email:'ktc24a31a0039@edu.kyoto-tech.ac.jp',
+    },
+
 }
+
+export const skills: skill[]=[
+
+{
+
+     name: 'Unity',
+    version: '6000.3.8f1',
+    level: 70,
+    category: 'game',
+    capabilities: ['3D/2Dゲーム制作', '', ''],
+    note: 'チーム制作で主にプレイヤー操作部分をメインで担当',
+},
+{
+    name:'',
+    version:'',
+    level:70,
+    category:'',
+    capabilities:[],
+    note:'',
+},
+]
